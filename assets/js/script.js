feee87e77dd8a7ecfe0f8e1f0736d5d6214827e6
Keenan <felipekeenan2000@gmail.com>
2024-02-23 17:32:16 -0300

    added responsiveness to the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
 let menu = document.querySelector('.menu-opener')
 menu.addEventListener('click', abrirMenu)
 let nav = document.querySelector('.menu nav')
+
 function abrirMenu() {
-    if (window.innerWidth <= 800) {
-        if (nav.style.display === 'none') {
-            nav.style.display = 'block'
-        }
-        else {
-            nav.style.display = 'none';
-        }
+    if (nav.style.display === 'none' || nav.style.display === '') {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
     }
 }
+
+// Adicione um evento de redimensionamento para garantir que o menu seja exibido ou ocultado corretamente quando a largura da tela mudar.
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 800) {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
+    }
+});
